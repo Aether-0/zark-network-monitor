5822ad6d9b9af22b8f2969e69e1de444bf484246
ZawwanZ <143575444+ZawwanZ@users.noreply.github.com>
2023-10-18 17:08:43 +0630

    Update zark.py

--- zark.py
+++ zark.py
@@ -1,66 +1,81 @@
+#!/usr/bin/python
+
 from scapy.all import *
 import socket
 import datetime
 from colorama import init, Fore
 import pyfiglet
 import os
+
 init(autoreset=True)
-llllIIIlIIIllll = pyfiglet.Figlet()
-llIIIIIlIlll = input("Do you want to log network traffic to a file? (y/n): ").strip().lower()
-if llIIIIIlIlll == "y":
-    IIIIIIlIIIIlIllIlI = input("Enter your log file:")
-    if not os.path.exists(IIIIIIlIIIIlIllIlI):
-        with open(IIIIIIlIIIIlIllIlI, "w"):
+figlet = pyfiglet.Figlet()
+
+log_choice = input("Do you want to log network traffic to a file? (y/n): ").strip().lower()
+
+if log_choice == "y":
+    log_file = input("Enter your log file:")
+    if not os.path.exists(log_file):
+        with open(log_file, "w"):
             pass
 else:
-    IIIIIIlIIIIlIllIlI = None
-def IIIllIIIllIIIIlII(IlIlllIIIlll):
-    if IIIIIIlIIIIlIllIlI:
-        with open(IIIIIIlIIIIlIllIlI, "a") as lIIlIIllIllllIllI:
-            lIIlIIllIllllIllI.write(IlIlllIIIlll + "\n")
-def IIIIIllllI(IlIlllIl):
-    IlllllllllIIIIlIl = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    if IlIlllIl.haslayer(IP):
-        lIIllIllIIlIIIlIIl = IlIlllIl[IP]
-        if socket.gethostbyname(socket.gethostname()) == lIIllIllIIlIIIlIIl.dst:
-            llllIlIIll = Fore.LIGHTGREEN_EX + "Incoming" + Fore.RESET
-        elif socket.gethostbyname(socket.gethostname()) == lIIllIllIIlIIIlIIl.src:
-            llllIlIIll = Fore.LIGHTBLUE_EX + "Outgoing" + Fore.RESET
+    log_file = None
+
+def log_packet(packet_info):
+    if log_file:
+        with open(log_file, "a") as f:
+            f.write(packet_info + "\n")
+
+def format_packet_info(pkt):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    if pkt.haslayer(IP):
+        ip_packet = pkt[IP]
+        
+        if socket.gethostbyname(socket.gethostname()) == ip_packet.dst:
+            direction = Fore.LIGHTGREEN_EX + "Incoming" + Fore.RESET
+        elif socket.gethostbyname(socket.gethostname()) == ip_packet.src:
+            direction = Fore.LIGHTBLUE_EX + "Outgoing" + Fore.RESET
         else:
-            llllIlIIll = Fore.LIGHTYELLOW_EX + "Unknown" + Fore.RESET
-        IIlIlIlIIIlIll = IlIlllIl.src
-        llIIIllllllIlIllIllI = IlIlllIl.dst
-        lIlIllIII = len(IlIlllIl)
-        llllIIllllIlIlIlIIll = lIIllIllIIlIIIlIIl.proto
-        if IlIlllIl.haslayer(TCP):
-            lIllIlIlIllIlIlIIl = "TCP"
-        elif IlIlllIl.haslayer(UDP):
-            lIllIlIlIllIlIlIIl = "UDP"
+            direction = Fore.LIGHTYELLOW_EX + "Unknown" + Fore.RESET
+        
+        mac_src = pkt.src
+        mac_dst = pkt.dst
+        packet_size = len(pkt)
+        protocol = ip_packet.proto
+        
+        if pkt.haslayer(TCP):
+            protocol_name = "TCP"
+        elif pkt.haslayer(UDP):
+            protocol_name = "UDP"
         else:
-            lIllIlIlIllIlIlIIl = "Other"
-        IlIlllIIIlll = (
-            lIIlIIllIllllIllI"{Fore.CYAN}Timestamp: {IlllllllllIIIIlIl}\n"
-            lIIlIIllIllllIllI"Direction: {llllIlIIll}\n"
-            lIIlIIllIllllIllI"Source IP: {Fore.LIGHTCYAN_EX}{lIIllIllIIlIIIlIIl.src}{Fore.RESET}\n"
-            lIIlIIllIllllIllI"Destination IP: {Fore.LIGHTCYAN_EX}{lIIllIllIIlIIIlIIl.dst}{Fore.RESET}\n"
-            lIIlIIllIllllIllI"Source MAC: {Fore.LIGHTMAGENTA_EX}{IIlIlIlIIIlIll}{Fore.RESET}\n"
-            lIIlIIllIllllIllI"Destination MAC: {Fore.LIGHTMAGENTA_EX}{llIIIllllllIlIllIllI}{Fore.RESET}\n"
-            lIIlIIllIllllIllI"Packet Size: {Fore.LIGHTMAGENTA_EX}{lIlIllIII} bytes{Fore.RESET}\n"
-            lIIlIIllIllllIllI"Protocol: {Fore.LIGHTMAGENTA_EX}{llllIIllllIlIlIlIIll_name}{Fore.RESET}\n"
+            protocol_name = "Other"
+        
+        packet_info = (
+            f"{Fore.CYAN}Timestamp: {timestamp}\n"
+            f"Direction: {direction}\n"
+            f"Source IP: {Fore.LIGHTCYAN_EX}{ip_packet.src}{Fore.RESET}\n"
+            f"Destination IP: {Fore.LIGHTCYAN_EX}{ip_packet.dst}{Fore.RESET}\n"
+            f"Source MAC: {Fore.LIGHTMAGENTA_EX}{mac_src}{Fore.RESET}\n"
+            f"Destination MAC: {Fore.LIGHTMAGENTA_EX}{mac_dst}{Fore.RESET}\n"
+            f"Packet Size: {Fore.LIGHTMAGENTA_EX}{packet_size} bytes{Fore.RESET}\n"
+            f"Protocol: {Fore.LIGHTMAGENTA_EX}{protocol_name}{Fore.RESET}\n"
         )
-        return IlIlllIIIlll
-def IllIlIIllIIIlIIllIl(IlIlllIl):
-    IlIlllIIIlll = IIIIIllllI(IlIlllIl)
-    if IlIlllIIIlll:
-        IIIllIIIllIIIIlII(IlIlllIIIlll)
-        print(IlIlllIIIlll)
-        print("-" * 60)  
+        return packet_info
+
+def network_monitoring(pkt):
+    packet_info = format_packet_info(pkt)
+    
+    if packet_info:
+        log_packet(packet_info)
+        print(packet_info)
+        print("-" * 60)  # Add a separator for better readability
+
 if __name__ == '__main__':
-    lIIIlIIIl = llllIIIlIIIllll.renderText("zark")
+    banner = figlet.renderText("zark")
     print(Fore.RED + "Author:            Zaw Wanz" + Fore.RESET)
-    print(Fore.MAGENTA + lIIIlIIIl + Fore.RESET)
+    print(Fore.MAGENTA + banner + Fore.RESET)
     print("Network Monitoring Started. Press Ctrl+C to stop.")
     try:
-        sniff(prn=IllIlIIllIIIlIIllIl)
+        sniff(prn=network_monitoring)
     except KeyboardInterrupt:
         print("Network Monitoring Stopped.")
